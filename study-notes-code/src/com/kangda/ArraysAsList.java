18be21263e91cc5ae74182e5e00a7efa549d9450
kangda28028 <kangda28028@hundsun.com>
2019-08-19 15:04:39 +0800

    [fix]:消除不必要的注释

--- study-notes-code/src/com/kangda/ArraysAsList.java
+++ study-notes-code/src/com/kangda/ArraysAsList.java
@@ -4,15 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/*********************************
- * <p> 文件名称: ArraysAsList
- * <p> 系统名称：
- * <p> 模块名称：com.kangda
- * <p> 功能说明: 
- * <p> 开发人员：kangda
- * <p> 开发时间：2019/8/16 14:51
- * <p> 修改记录：程序版本   修改日期   修改人员   修改单号   修改说明
- **********************************/
 public class ArraysAsList {
     public static void main(String[] args) {
         String[] a = {"1","2","h"};
--- study-notes-code/src/com/kangda/CheckPhoneNumber.java
+++ study-notes-code/src/com/kangda/CheckPhoneNumber.java
@@ -2,15 +2,6 @@ package com.kangda;
 
 import java.util.regex.Pattern;
 
-/*********************************
- * <p> 文件名称: CheckPhoneNumber
- * <p> 系统名称：
- * <p> 模块名称：com.kangda
- * <p> 功能说明: 校验手机号码格式
- * <p> 开发人员：kangda28028
- * <p> 开发时间：2019/8/19 14:53
- * <p> 修改记录：程序版本   修改日期   修改人员   修改单号   修改说明
- **********************************/
 public class CheckPhoneNumber {
     public static boolean isPhoneNumber(String phoneNumber) {
         boolean flag = false;
