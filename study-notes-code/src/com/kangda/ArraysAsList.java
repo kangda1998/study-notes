7dc41a9aac71c7f34afd9112b38e55d57be9c46a
kangda28028 <kangda28028@hundsun.com>
2019-09-27 14:16:51 +0800

    [feat]:增加long的练习

--- study-notes-code/src/com/kangda/ArraysAsList.java
+++ study-notes-code/src/com/kangda/ArraysAsList.java
@@ -8,7 +8,9 @@ public class ArraysAsList {
     public static void main(String[] args) {
         String[] a = {"1","2","h"};
        int[] i ={2,3,4};
-
+    long b = 2L;
+    long c = b-1L;
+        System.out.println(c);
 //        ArrayList arrayList = new ArrayList(Arrays.asList(a));
       List list = Arrays.asList(i); //返回的是ArrayList的一个内部类，要变成ArrayList需要像21行那样
         System.out.println(list.size()); //当为基本数据类型size=1，而传入包装类，Arrays.asList()为3
